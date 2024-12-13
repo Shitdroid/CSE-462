242396c334ac09c2ba92144a21ddceb590b3d566
BarneyStinson73 <92513392+BarneyStinson73@users.noreply.github.com>
2024-12-13 20:04:41 +0600

    Corrected bfs.cpp

--- Project-MLST/Codes/bfs.cpp
+++ Project-MLST/Codes/bfs.cpp
@@ -1,33 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    vector<vector<int>> adj(n);
-    vector<int> degree(n, 0);
-
-    for (int i = 0; i < m; ++i) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-        degree[u]++;
-        degree[v]++;
+class node{
+    public:
+    int data;
+    bool isVisited;
+    vector<node*> children;
+    bool isLeaf;
+    node(int data){
+        this->data = data;
+        this->isVisited = false;
+        this->isLeaf = true;
     }
-
+};
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<node*> nodes;
+    for(int i=0;i<n;i++){
+        node* temp = new node(i);
+        nodes.push_back(temp);
+    }
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        nodes[u]->children.push_back(nodes[v]);
+        nodes[v]->children.push_back(nodes[u]);
+    }
+    queue<node*> q;
+    q.push(nodes[0]);
+    nodes[0]->isVisited = true;
+    // int leafCount = 0;
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        for(int i=0;i<temp->children.size();i++){
+            if(!temp->children[i]->isVisited){
+                q.push(temp->children[i]);
+                temp->isLeaf = false;
+                temp->children[i]->isVisited = true;
+            }
+        }
+    }
     int leafCount = 0;
-    for (int i = 0; i < n; ++i) {
-        if (degree[i] == 1) {
+    for(int i=0;i<n;i++){
+        if(nodes[i]->isLeaf){
             leafCount++;
         }
     }
-
-    cout << leafCount << endl;
-
-    return 0;
+    cout<<leafCount<<endl;
 }
