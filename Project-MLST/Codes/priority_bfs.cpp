a952ba5201391c8389aa0f4bca52c07f84168102
BarneyStinson73 <92513392+BarneyStinson73@users.noreply.github.com>
2024-12-14 02:44:48 +0600

    Update priority_bfs.cpp

--- Project-MLST/Codes/priority_bfs.cpp
+++ Project-MLST/Codes/priority_bfs.cpp
@@ -1,55 +1,68 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <functional>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    vector<vector<int>> adj(n);
-    vector<int> degree(n, 0);
-
-    for (int i = 0; i < m; ++i) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-        degree[u]++;
-        degree[v]++;
+class node{
+    public:
+    int data;
+    bool isVisited;
+    vector<node*> children;
+    bool isLeaf;
+    int priority;
+    node(int data){
+        this->data = data;
+        this->isVisited = false;
+        this->isLeaf = true;
     }
-
-    auto cmp = [&](int a, int b) {
-        return degree[a] > degree[b];
+};
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<node*> nodes;
+    for(int i=0;i<n;i++){
+        node* temp = new node(i);
+        nodes.push_back(temp);
+    }
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        nodes[u]->children.push_back(nodes[v]);
+        nodes[v]->children.push_back(nodes[u]);
+    }
+    auto compare = [](node* a, node* b) {
+        return a->priority < b->priority;
     };
 
-    priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
-    vector<bool> visited(n, false);
+    priority_queue<node*, vector<node*>, decltype(compare)> pq(compare);
 
-    pq.push(0);
-    visited[0] = true;
-
-    int leafCount = 0;
+    nodes[0]->priority = nodes[0]->children.size();
+    pq.push(nodes[0]);
+    nodes[0]->isVisited = true;
 
     while (!pq.empty()) {
-        int node = pq.top();
+        node* current = pq.top();
         pq.pop();
 
-        if (degree[node] == 1) {
-            leafCount++;
+        if (current->isVisited) continue;
+        current->isVisited = true;
+
+        if (current->children.empty()) {
+            current->isLeaf = true;
+        } else {
+            current->isLeaf = false;
         }
 
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                pq.push(neighbor);
-                visited[neighbor] = true;
+        for (auto child : current->children) {
+            if (!child->isVisited) {
+                child->priority = current->priority - child->children.size();
+                pq.push(child);
+                child->isVisited = true;
             }
         }
     }
-
-    cout << leafCount << endl;
-
-    return 0;
+    int leafCount = 0;
+    for(int i=0;i<n;i++){
+        if(nodes[i]->isLeaf){
+            leafCount++;
+        }
+    }
+    cout<<leafCount<<endl;
 }
